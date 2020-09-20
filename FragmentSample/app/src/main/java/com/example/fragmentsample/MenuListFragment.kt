0e0f915e8e212fb3a5ab6d5e91de8ddff4f2fa31
hhiroki <gyouzakuuka@gmail.com>
2020-09-20 19:50:43 +0900

    add landscape mode complete

--- FragmentSample/app/src/main/java/com/example/fragmentsample/MenuListFragment.kt
+++ FragmentSample/app/src/main/java/com/example/fragmentsample/MenuListFragment.kt
@@ -10,26 +10,17 @@ import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    /**
+     * 同一画面に注文完了のフレームレイアウトが存在するかどうかで画面サイズを判定するプロパティ
+     */
+    private var _isLayoutXLarge = true
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val menuThanksFrame = activity?.findViewById<View>(R.id.menuThanksFrame)
+        if (menuThanksFrame == null) {
+            _isLayoutXLarge = false
         }
     }
 
@@ -62,31 +53,22 @@ class MenuListFragment : Fragment() {
             val item = parent.getItemAtPosition(position) as MutableMap<String, String>
             val menuName = item["name"]
             val menuPrice = item["price"]
-            val intent = Intent(activity, MenuThanksActivity::class.java)
-            intent.putExtra("menuName", menuName)
-            intent.putExtra("menuPrice", menuPrice)
-            startActivity(intent)
-        }
-    }
+            val bundle = Bundle()
+            bundle.putString("menuName", menuName)
+            bundle.putString("menuPrice", menuPrice)
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            if (_isLayoutXLarge) {
+                val transaction = fragmentManager?.beginTransaction()
+                val menuThanksFragment = MenuThanksFragment()
+                menuThanksFragment.arguments = bundle
+                transaction?.replace(R.id.menuThanksFrame, menuThanksFragment)
+                transaction?.commit()
             }
+            else {
+                val intent = Intent(activity, MenuThanksActivity::class.java)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+        }
     }
 }
--- FragmentSample/app/src/main/java/com/example/fragmentsample/MenuThanksFragment.kt
+++ FragmentSample/app/src/main/java/com/example/fragmentsample/MenuThanksFragment.kt
@@ -8,26 +8,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuThanksFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuThanksFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _isLayoutXLarge = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val menuListFragment = fragmentManager?.findFragmentById(R.id.fragmentMenuList)
+        if (menuListFragment == null) {
+            _isLayoutXLarge = false
         }
     }
 
@@ -36,9 +24,16 @@ class MenuThanksFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_menu_thanks, container, false)
+        val extras: Bundle?
 
-        val intent = activity?.intent
-        val extras = intent?.extras
+        if (_isLayoutXLarge) {
+            extras = arguments
+        }
+        else {
+            val intent = activity?.intent
+            extras = intent?.extras
+        }
+
         val menuName = extras?.getString("menuName")
         val menuPrice = extras?.getString("menuPrice")
         val tvMenuName = view.findViewById<TextView>(R.id.tvMenuName)
@@ -54,27 +49,14 @@ class MenuThanksFragment : Fragment() {
 
     private inner class ButtonClickListener : View.OnClickListener {
         override fun onClick(view: View) {
-            activity?.finish()
-        }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuThanksFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuThanksFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            if (_isLayoutXLarge) {
+                val transaction = fragmentManager?.beginTransaction()
+                transaction?.remove(this@MenuThanksFragment)
+                transaction?.commit()
             }
+            else {
+                activity?.finish()
+            }
+        }
     }
 }
