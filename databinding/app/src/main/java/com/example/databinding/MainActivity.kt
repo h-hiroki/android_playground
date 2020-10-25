789b416b7f938cc982557c672f7861d38e649fdf
hhiroki <gyouzakuuka@gmail.com>
2020-10-25 22:33:25 +0900

    refactor

--- databinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ databinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -8,13 +8,24 @@ import com.example.databinding.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        initSample(binding)
+        initCounter(binding)
+    }
+
+    private fun initSample(binding: ActivityMainBinding) {
         binding.sampleTextView.text = "Hello init Binding!"
-        binding.sampleButton.setOnClickListener {
-            binding.sampleTextView.text = "Hello Button Binding!"
+        binding.bindbutton.setOnClickListener {
+            binding.sampleTextView.text = "Button bind success!!"
+        }
+    }
+
+    private fun initCounter(binding: ActivityMainBinding) {
+        binding.counter = 1
+        binding.countUpButton.setOnClickListener {
+            binding.counter += 1
         }
     }
 }
