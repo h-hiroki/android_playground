eef322d9a3ba6477f5eca2240a99d6468d9c9720
hhiroki <gyouzakuuka@gmail.com>
2020-11-01 15:47:17 +0900

    add disposableobserver

--- RxDemo1/app/src/main/java/com/example/rxdemo1/MainActivity.java
+++ RxDemo1/app/src/main/java/com/example/rxdemo1/MainActivity.java
@@ -10,6 +10,7 @@ import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,11 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private final static String TAG = "myApp";
     private String greeting = "Hello From RxJava";
     private Observable<String> myObservable;
-    private Observer<String> myObserver;
+    private DisposableObserver<String> myObserver;
 
     private TextView textView;
 
-    private Disposable disposable;
+//    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +34,31 @@ public class MainActivity extends AppCompatActivity {
         myObservable.subscribeOn(Schedulers.io());
         myObservable.observeOn(AndroidSchedulers.mainThread());
 
-        myObserver = new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.i(TAG, "onSubscribe invoked");
-                disposable = d;
-            }
+//        myObserver = new Observer<String>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.i(TAG, "onSubscribe invoked");
+//                disposable = d;
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                Log.i(TAG, "onNext invoked");
+//                textView.setText(s);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.i(TAG, "onError invoked");
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.i(TAG, "onComplete invoked");
+//            }
+//        };
 
+        myObserver = new DisposableObserver<String>() {
             @Override
             public void onNext(String s) {
                 Log.i(TAG, "onNext invoked");
@@ -57,12 +76,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+
         myObservable.subscribe(myObserver);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        disposable.dispose();
+//        disposable.dispose();
+
+        myObserver.dispose();
     }
 }
